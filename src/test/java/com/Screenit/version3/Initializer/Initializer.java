5ec303388f5a448b93826fe9b68e415ac5f66400
vipersathish <114578040+vipersathish@users.noreply.github.com>
2023-08-07 11:45:54 +0530

    Update Initializer.java

--- src/test/java/com/Screenit/version3/Initializer/Initializer.java
+++ src/test/java/com/Screenit/version3/Initializer/Initializer.java
@@ -6,10 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.EmailAttachment;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.MultiPartEmail;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
