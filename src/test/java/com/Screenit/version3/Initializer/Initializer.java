822aec9a796f1e0dfe4b6405d0641af0cfd83c30
vipersathish <114578040+vipersathish@users.noreply.github.com>
2023-08-09 18:21:58 +0530

    Update Initializer.java

--- src/test/java/com/Screenit/version3/Initializer/Initializer.java
+++ src/test/java/com/Screenit/version3/Initializer/Initializer.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriver.SystemProperty;
+
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import org.testng.Reporter;
